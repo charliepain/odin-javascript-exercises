1b2c1a33a81ce40909ff2c923a7d8accb57067b0
Charlie <charliepeng222@gmail.com>
2024-05-23 23:04:43 -0400

    Finish exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, occurences) {
+    if (occurences < 0) return "ERROR";
+    repeatedString = "";
+    for (let i = 0; i < occurences; i++) repeatedString += string;
+    return repeatedString;
 };
 
 // Do not edit below this line
