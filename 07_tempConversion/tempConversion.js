ffab56c939c53c2ffad7ed716c7274c1e43db304
Charlie <charliepeng222@gmail.com>
2024-05-24 11:10:35 -0400

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+  let celsiusTemperature = (fahrenheitTemperature - 32) * (5 / 9);
+  return Number(celsiusTemperature.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  let fahrenheitTemperature = (celsiusTemperature * (9 / 5)) + 32;
+  return Number(fahrenheitTemperature.toFixed(1));
 };
 
 // Do not edit below this line
