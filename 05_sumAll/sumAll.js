0caf4015a8ab9a79b0bc02e0f65814252bc80fff
Charlie <charliepeng222@gmail.com>
2024-05-24 10:25:25 -0400

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+    if (typeof number1 !== "number"
+        || typeof number2 !== "number"
+        || number1 < 0
+        || number2 < 0
+    ) return "ERROR";
 
+    if (number1 > number2) {
+        let temporary1 = number1;
+        number1 = number2;
+        number2 = temporary1;
+    }
+
+    let sum = 0;
+    for (let i = number1; i <= number2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
