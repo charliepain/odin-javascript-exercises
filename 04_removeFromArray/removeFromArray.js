d1407e56bb9d6a92b81a76eb1cc3fdc87253e0d6
Charlie <charliepeng222@gmail.com>
2024-05-24 00:03:32 -0400

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...targets) {
+    for (let i = 0; i < array.length; i++) {
+        for (const target of targets) {
+            if (array[i] === target) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
