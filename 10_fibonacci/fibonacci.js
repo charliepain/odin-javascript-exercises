e4a314b52fc6886b7dc7fb6265528e13083c0fa7
Charlie <charliepeng222@gmail.com>
2024-05-31 17:06:33 -0400

    Finish 10_fibonacci exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) return "OOPS"
+    if (n <= 1) return n;
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= n; i++) {
+        [a, b] = [b, b + a];
+    }
+    return b;
 };
 
 // Do not edit below this line
