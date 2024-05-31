8a1cc188b6590c384d7b0ea881bb6965c604ab0b
Charlie <charliepeng222@gmail.com>
2024-05-31 17:35:29 -0400

    Finish 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+        let currentYear = (new Date()).getFullYear();
+        let oldestAge =
+            ("yearOfDeath" in oldest) ? oldest.yearOfDeath - oldest.yearOfBirth
+            : currentYear - oldest.yearOfBirth;
+        let personAge =
+            ("yearOfDeath" in person) ? person.yearOfDeath - person.yearOfBirth
+            : currentYear - person.yearOfBirth;
+        return (personAge > oldestAge) ? person : oldest;
+    });
 };
 
 // Do not edit below this line
