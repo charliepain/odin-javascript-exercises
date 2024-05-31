ed120ba77917afb5f67a6e693062552cebab6614
Charlie <charliepeng222@gmail.com>
2024-05-31 16:41:29 -0400

    Finish 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let arrayString = string.split("");
+    let arrayStringLetters = arrayString.filter(char => {
+        return !`!?\\/ (),.;'"`.includes(char);
+    });
+    let arrayStringLower = arrayStringLetters.map(char => char.toLowerCase());
+    let modifiedString = arrayStringLower.join("");
+    let reverseModifiedString = arrayStringLower.reverse().join("");
+    return modifiedString === reverseModifiedString;
 };
 
 // Do not edit below this line
